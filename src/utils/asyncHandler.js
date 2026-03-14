fd259794721f0fd80d564e745d23d7d0346e83b0
Abhay-0103 <ab0321054@gmail.com>
2026-03-15 01:20:33 +0530

    Add asyncHandler utility for handling asynchronous requests

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,38 @@
+// Using Promise's
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export {asyncHandler}
+
+
+
+/* same menthod used down in a single-line
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async() => {}
+
+*/
+
+
+/* Used Normal TRY-CATCH SATEMENT
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+*/
